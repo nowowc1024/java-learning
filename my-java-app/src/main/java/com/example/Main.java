db9de36ec6fe9e4b98292b7f2d88b37dc8a4875a
nowowc1024 <cn150891@student.sgh.waw.pl>
2026-03-14 21:01:52 +0000

    Java Code update

--- my-java-app/src/main/java/com/example/Main.java
+++ my-java-app/src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello, Java!");
-            System.out.println("Welcome to Maven!");
-}
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "! Welcome to Maven!");
+
+        // Loop until user types "exit"
+        while (true) {
+            System.out.print("Type a message (or 'exit' to quit): ");
+            String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                System.out.println("Goodbye, " + name + "!");
+                break;
+            }
+
+            System.out.println("You said: " + input);
+        }
+
+        scanner.close();
+    }
 }
